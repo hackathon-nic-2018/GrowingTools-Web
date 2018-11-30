3d08fa2d48393b57eaadbd172b7146ded3925de7
Idsarth Jr <idsarthjr19@gmail.com>
2018-11-30 00:38:49 -0600

    creando la vista empleados

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,4 +13,15 @@ router.get('/login', (request, response) => {
     response.render('login');
 })
 
+
+router.get('/dashboard', (request, response) => {
+    response.render('dashboard');
+})
+
+router.get('/empleados', (request, response) => {
+    response.render('empleados');
+})
+
+
+
 module.exports = router;
