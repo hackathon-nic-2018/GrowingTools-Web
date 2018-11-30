8a3ccdb0838eb838bd28574aa5c2429aa285ce76
Idsarth Jr <idsarthjr19@gmail.com>
2018-11-30 03:10:18 -0600

    configurando las rutas de la api

--- src/routes/api.js
+++ src/routes/api.js
@@ -27,5 +27,54 @@ router.get('/api/Empresa', (request,res,next) => {
     });
 });
 
+router.get('/api/Inventario', function(request,res, next){
+    let req = new mssql.Request();
+    req.query(`select I.ID_inventario,I.Precio_venta,I.Costo,C.Categoria,Cantidad,I.Nombre,P.NombreProveedor
+    from Inventario as I
+    inner join Categoria as C on C.ID_categoria = I.ID_categoria
+    inner join Proveedores as P on P.ID_proveedor = I.ID_proveedor
+    where I.ID_empresa = (select Emp.ID_empresa from Empresas as Emp where Empresa = 'Paisanos')`,function(err, result){
+        if(err) {
+            return next(err);
+        }
+            
+        let data={};
+        data = result.recordset;
+        res.send(data);    
+    });
+});
+// ------------------------
+router.get('/api/Facturas', function(request,res,next){
+    let req=new mssql.Request();
+    req.query('SELECT * FROM FacturasT',function(err,result){
+        if(err)  
+            return next(err);
+        data = result.recordset;
+        res.send(data);    
+    })
+});
+
+router.get('/api/Historial',function(request,res,next){
+    var req = new mssql.Request();
+    req.query('SELECT * FROM Historial_movimientos_inventario',function(err,result){
+        if(err)
+            return next(err)
+        data = result.recordset;
+        res.send(data);
+    })
+})
+
+
+router.get('/api/Usuarios', function(request, res,next){
+    let req=new mssql.Request();
+    req.query('SELECT * FROM Usuarios',function(err,result){
+    if(err)
+        return next(err);
+    data = result.recordset;
+    res.send(data);    
+    });
+})
+
+
 module.exports = router;
 
