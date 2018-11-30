d9955f9d7cdb7b663a7bb7516c54b92f9c08c76b
Idsarth Jr <idsarthjr19@gmail.com>
2018-11-29 23:41:42 -0600

    errores corregidos, fidel mucho atrasa

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path')
 const handlebars = require('express-handlebars')
-const mssql=require('mssql');
 const app = express();
-const router=require('./routes/rutas');
-const config=require('./model/config')
+
 //  Settings
 app.set('PORT', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
@@ -23,27 +21,11 @@ app.set('view engine', '.hbs')
 
 // Routes
 app.use(require('./routes/index'));
-app.use(router);
-
 
 //  Static Files
 app.use(express.static(path.join(__dirname, 'assets')))
 
-app.use(function (req, res, next) {
-    var err = new Error('No Encontrado: '+ req.method + ":" + req.originalUrl);
-    err.status = 404;
-    next(err);
-});
-
 //  Server is listenning
 app.listen(app.get('PORT'), () => {
     console.log(`Server on PORT ${app.get('PORT')}`)
-});
-
-const connection=mssql.connect(config,function(err,res){
-    if(err){
-        throw err;
-    }else{
-        console.log('Base de datos Activada');
-    }   
 });
